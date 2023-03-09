2bbdee2f6ace13b3d8fdec8438feb4445ad147c0
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2023-03-09 07:45:32 +0700

    disable get /users

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,7 +3,7 @@ const userHandler = require('../handlers/userHandler');
 
 const router = express.Router();
 
-router.get('/users', userHandler.getHandler);
+// router.get('/users', userHandler.getHandler);
 router.post('/users', userHandler.postHandler);
 
 module.exports = router;
